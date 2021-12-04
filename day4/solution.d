ce575b69d75f75dec81954c137935a459dc6412f
Martijn van Iersel <mvaniersel@gmail.com>
2021-12-04 11:33:05 +0100

    Refactor solution 4

--- day4/solution.d
+++ day4/solution.d
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S rdmd -unittest -I..
+#!/usr/bin/env -S rdmd -I..
 
 import common.io;
 import std.stdio;
@@ -6,75 +6,71 @@ import std.string;
 import std.conv;
 import std.algorithm;
 import std.array;
+import std.range;
+import std.functional;
 
-int[] fiveInts(string line) {
-	int[] result = [];
-	for (int i = 0; i < 5; ++i) {
-		string val = line[i*3..(i*3)+2].strip();
-		result ~= to!int(val);
-	}
-	return result;
+alias Board = int[][];
+struct Winner {
+	Board board;
+	int number; // the number that made this board win
 }
-void main() {	
-	string[] data = readLines("input");
-	int[] numbers = data[0].split(",").map!(to!int).array;
-	writeln(numbers);
-	
-	int [][][] boards = [];
 
-	for (int i = 2; i < data.length; i += 6) {
-		int[][] board = data[i..i+5].map!(line => fiveInts(line)).array;
-		boards ~= board;
+Board parseBoard(string[] lines) {
+	assert(lines.length == 5);
+	return lines.map!(line => line.split.map!(to!int).array).array;
+}
+
+bool isBingo(Board board) {
+	for (int i = 0; i < 5; ++i) {
+		if (board[i].sum == 0 || board.transversal(i).sum == 0) {
+			return true;
+		}
 	}
+	return false;
+}
 
+// find called number on board, replace with 0;
+void markNumber(Board board, int number) {
+	foreach (int[] row; board) {
+		foreach (ref int cell; row) {
+			if (cell == number) cell = 0;
+		}
+	}
+}
+
+Winner[] sortedWinners(Board[] boards, int[] numbers) {
+	Winner[] winners = [];
 	foreach (int number; numbers) {
-		// find on board, replace with 0;
-
-		foreach (int[][] board; boards) {
-			foreach (int[] row; board) {
-				foreach (ref int cell; row) {
-					if (cell == number) cell = 0;
-				}
-			}
+		foreach (Board board; boards) {
+			board.markNumber(number);		
 		}
 
-		// check boards for winners
-		bool found = false;
-		int[][] winningBoard;
-		foreach (int[][] board; boards) {
-			foreach(int[] row; board) {
-				if (row.sum() == 0) {
-					found = true;
-					winningBoard = board;
-					break;
-				}
-			}
+		// add winning boards to winners set		
+		winners ~= boards.filter!isBingo.map!(b => Winner(b, number)).array;
+		// remove winning boards from play
+		boards = boards.filter!(not!isBingo).array;
+	}
+	return winners;
+}
 
-			for (int y = 0; y < 5; ++y) {
-				if (board.map!(row => row[y]).sum() == 0) {
-					found = true;
-					winningBoard = board;
-					break;
-				}
-			}
+int score(Winner winner) {
+	return winner.number * winner.board.map!(row => row.sum()).sum();
+}
 
-			if (found) {
-				writeln("Winning board: ", winningBoard);
-				int totalSum = winningBoard.map!(row => row.sum()).sum();
-
-				writeln(totalSum, " ", number, " ", totalSum * number);
-				
-				boards = boards.filter!(b => b != winningBoard).array;
-				writeln("Remaining: ", boards.length);
-			}
-		}
-
-		// if (boards.length == 1) {
-		// 	break;
-		// }
-
+int[2] firstAndLastScores(string fname) {	
+	string[] data = readLines(fname);
+	int[] numbers = data[0].split(",").map!(to!int).array;
+	
+	Board[] boards = [];
+	for (int i = 2; i < data.length; i += 6) {
+		boards ~= parseBoard(data[i..i+5]);
 	}
 
+	Winner[] winners = sortedWinners(boards, numbers);
+	return [winners[0].score, winners[$-1].score];
+}
 
-
+void main() {
+	assert (firstAndLastScores("test") == [ 4512, 1924 ]);
+	writeln (firstAndLastScores("input"));
 }
