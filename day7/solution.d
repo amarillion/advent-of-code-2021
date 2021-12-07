1ad6bfdf478e278a486ee0ceb78a9312f2855e8c
Martijn van Iersel <mvaniersel@gmail.com>
2021-12-07 07:55:16 +0100

    Day 7 part 2

--- day7/solution.d
+++ day7/solution.d
@@ -10,7 +10,7 @@ import std.array;
 import std.concurrency;
 import std.math;
 
-int optimalPos(int[] positions) {
+int optimalPos(int[] positions, int delegate(int) fuelCost) {
 	writefln ("Total: %s count %s ", positions.sum, positions.length);
 	writeln ("Avg: ", cast(double)positions.sum / cast(double)positions.length);
 	writefln ("Min: %s, Max: %s ", positions.minElement, positions.maxElement);
@@ -22,7 +22,7 @@ int optimalPos(int[] positions) {
 	for (int level = positions.minElement; level <= positions.maxElement; ++level) {
 		int cost = 0;
 		foreach (pos; positions) {
-			cost += abs(level - pos);
+			cost += fuelCost(abs(level - pos));
 			// writefln("Move from %s to %s: %s fuel", pos, level, abs(level - pos));
 		}
 		if (first || cost < minCost) {
@@ -40,12 +40,21 @@ auto solve (string fname) {
 	string line = readLines(fname)[0];
 	int[] positions = line.split(",").map!(to!int).array;
 
+	int[] fuelCosts;
+	int prev = 0;
+	for (int i = 0; i <= positions.maxElement; ++i) {
+		prev += i; 
+		fuelCosts ~= prev;
+	}
+	writeln(fuelCosts);
+
 	return [
-		optimalPos(positions),
+		optimalPos(positions, i => i),
+		optimalPos(positions, i => fuelCosts[i]),
 	];
 }
 
 void main() {
-	assert (solve("test") == [ 37 ]);
+	assert (solve("test") == [ 37, 168 ]);
 	writeln (solve("input"));
 }
