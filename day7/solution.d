ff6352df9b64c3ec5a9de464e26a3f994b73a1b6
Martijn van Iersel <mvaniersel@gmail.com>
2021-12-07 07:59:17 +0100

    Day 7 cleanup

--- day7/solution.d
+++ day7/solution.d
@@ -10,12 +10,7 @@ import std.array;
 import std.concurrency;
 import std.math;
 
-int optimalPos(int[] positions, int delegate(int) fuelCost) {
-	writefln ("Total: %s count %s ", positions.sum, positions.length);
-	writeln ("Avg: ", cast(double)positions.sum / cast(double)positions.length);
-	writefln ("Min: %s, Max: %s ", positions.minElement, positions.maxElement);
-
-	int sum = positions.sum;
+int optimalLevel(int[] positions, int delegate(int) fuelCost = i => i) {
 	int minCost = 0;
 	int minLevel = 0;
 	bool first = true;
@@ -23,16 +18,13 @@ int optimalPos(int[] positions, int delegate(int) fuelCost) {
 		int cost = 0;
 		foreach (pos; positions) {
 			cost += fuelCost(abs(level - pos));
-			// writefln("Move from %s to %s: %s fuel", pos, level, abs(level - pos));
 		}
 		if (first || cost < minCost) {
 			minCost = cost;
 			minLevel = level;
 			first = false;
 		}
-		writefln("%s %s", level, cost);
 	}
-	writefln("Best: %s %s", minLevel, minCost);
 	return minCost;
 }
 
@@ -46,11 +38,10 @@ auto solve (string fname) {
 		prev += i; 
 		fuelCosts ~= prev;
 	}
-	writeln(fuelCosts);
 
 	return [
-		optimalPos(positions, i => i),
-		optimalPos(positions, i => fuelCosts[i]),
+		optimalLevel(positions),
+		optimalLevel(positions, i => fuelCosts[i]),
 	];
 }
 
