401abacf6a8798e0fea2259fd15ca86b5925c299
Martijn van Iersel <mvaniersel@gmail.com>
2021-12-21 10:08:16 -0600

    Day 21 code clean up

--- day21/solution.d
+++ day21/solution.d
@@ -14,9 +14,22 @@ import common.util;
 import common.grid;
 import common.coordrange;
 
-ulong part1(const int[] initialPos) {
-	int[] score = [ 0, 0 ];
-	int[] pos = initialPos.dup;
+struct State {
+	int[2] score = [0, 0];
+	int[2] pos;
+}
+
+State advanceState(const State input, int p, int advance) {
+	State result = input;
+	result.pos[p] += advance;
+	while(result.pos[p] > 10) result.pos[p] -= 10;
+	result.score[p] += result.pos[p];
+	// writefln("Player %s rolls %s and moves to space %s for a total score of %s", p, advance, result.pos[p], result.score[p]);
+	return result;
+}
+
+ulong part1(State initialState) {
+	State state = initialState; 
 
 	int die = 0;
 	int totalRolls = 0;
@@ -29,81 +42,62 @@ ulong part1(const int[] initialPos) {
 	bool done = false;
 	while(!done) {
 		for (int p = 0; p < 2; ++p) {
-			int advance = roll() + roll() + roll();
-			pos[p] += advance;
-			while(pos[p] > 10) pos[p] -= 10;
-			score[p] += pos[p];
-			// writefln("Player %s rolls %s and moves to space %s for a total score of %s", p, advance, pos[p], score[p]);
-			if (score[p] >= 1000) { done = true; break; }
+			state = advanceState(state, p, roll() + roll() + roll());
+			if (state.score[p] >= 1000) { done = true; break; }
 		}
 	}
 	
-	int loserScore = min(score[0], score[1]);
-	// writefln("%s %s", totalRolls, loserScore);
+	int loserScore = min(state.score[0], state.score[1]);
 	return totalRolls * loserScore;
 }
 
-struct State {
-	int[2] score;
-	int[2] pos;
-}
-
-ulong part2(const int[] initialPos) {
-	ulong[State] states;
-
-	State initialState;
-	initialState.score = [0, 0];
-	initialState.pos = [initialPos[0], initialPos[1]];
-	states[initialState] = 1;
-
-	State advanceState(const State input, int p, int[3] roll) {
-		State result = input;
-		int advance = roll[0] + roll[1] + roll[2];
-		result.pos[p] += advance;
-		while(result.pos[p] > 10) result.pos[p] -= 10;
-		result.score[p] += result.pos[p];
-		return result;
+auto generateDiracTriples() {
+	int[int] result;
+	for (int r1 = 1; r1 <= 3; ++r1) {
+		for (int r2 = 1; r2 <= 3; ++r2) {
+			for (int r3 = 1; r3 <= 3; ++r3) {
+				int roll = r1 + r2 + r3;
+				result[roll]++;
+			}
+		}
 	}
+	return result;
+}
+enum diracFrq = generateDiracTriples();
 
-	bool done = false;
+ulong part2(State initialState) {
+	ulong[State] states;
+	states[initialState] = 1;
 	ulong[int] wonCount;
 
-	while(!done) {
+	do {
 		for (int p = 0; p < 2; ++p) {
-			writeln(states);
 			ulong[State] newStates;
-			foreach (k, v; states) {
-				for (int r1 = 1; r1 <= 3; ++r1) {
-					for (int r2 = 1; r2 <= 3; ++r2) {
-						for (int r3 = 1; r3 <= 3; ++r3) {
-							State newState = advanceState(k, p, [r1, r2, r3]);
-							if (newState.score[p] >= 21) {
-								wonCount[p] += v;
-							}
-							else {
-								newStates[newState] += v;
-							}
-						}
+			foreach (state, stateCount; states) {
+				foreach(rollSum, rollFrq; diracFrq) {
+					State newState = advanceState(state, p, rollSum);
+					if (newState.score[p] >= 21) {
+						wonCount[p] += (stateCount * rollFrq);
+					}
+					else {
+						newStates[newState] += (stateCount * rollFrq);
 					}
 				}
 			}
 			states = newStates;
-			writeln(states);
 		}
-		if (states.length == 0) {
-			break;
-		}
-	}
+	} while(states.length > 0);
 
-	writeln(wonCount);
 	return max(wonCount[0], wonCount[1]);
 }
 
 auto solve (string fname) {
 	string[] lines = readLines(fname);
-	int[] pos = lines.map!(l => to!int(l[$-1..$])).array;
+	int[2] pos = lines.map!(l => to!int(l[$-1..$])).array;
+	State initialState;
+	initialState.pos = pos;
 
-	return [ part1(pos), part2(pos) ];
+	return [ part1(initialState), part2(initialState) ];
 }
 
 void main() {
