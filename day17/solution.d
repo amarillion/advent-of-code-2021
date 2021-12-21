c9b29c1b4cda9bef9021962daf6aa3933b81aaf5
Martijn van Iersel <mvaniersel@gmail.com>
2021-12-21 13:23:24 -0600

    Day 17: part 2

--- day17/solution.d
+++ day17/solution.d
@@ -24,13 +24,16 @@ auto solve (string fname) {
 	string line = readLines(fname)[0]["target area: ".length..$];
 	int[][] coords = line.split(", ").map!(s => s["x=".length..$].split("..").map!(to!int).array).array;
 	
-	Point topLeft = Point(coords[0][0], coords[1][0]);
-	Point bottomRight = Point(coords[0][1], coords[1][1]);
-	writeln(topLeft, bottomRight);
-	
-	int maxSuccess = int.min;
+	Point p1 = Point(coords[0][0], coords[1][0]);
+	Point p2 = Point(coords[0][1], coords[1][1]);
+	Point bottomLeft = Point(min(p1.x, p2.x), min(p1.y, p2.y));
+	Point topRight = Point(max(p1.x, p2.x), max(p1.y, p2.y));
+	// writeln(bottomLeft, topRight);
 
-	foreach(initialV; PointRange(Point(0, 0), Point(100, 100))) {
+	int maxSuccess = int.min;
+	Point[] successes = [];
+
+	foreach(initialV; PointRange(Point(0, -400), Point(400, 400))) {
 		Point v = initialV;
 
 		Point pos = Point(0, 0);
@@ -44,26 +47,36 @@ auto solve (string fname) {
 
 		int maxHeight = int.min;
 		bool hit = false;
-		while(pos.y >= bottomRight.y) {
+		while(pos.y >= bottomLeft.y) {
 			doStep();
-			hit |= (pos.inside(topLeft, bottomRight));
+			hit |= (pos.inside(bottomLeft, topRight));
 			if (pos.y > maxHeight) {
 				maxHeight = pos.y;
 			}
+			// writefln("Pos: %s, v: %s", pos, v);
 		}
 
-		writefln("InitialV: %s, hit: %s, maxHeight: %s", initialV, hit, maxHeight);
+		// writefln("InitialV: %s, hit: %s, maxHeight: %s", initialV, hit, maxHeight);
 
 		if (hit && maxHeight > maxSuccess) {
 			maxSuccess = maxHeight;
 		}
+		if (hit) { successes ~= initialV; }
 	}
 
-	return [ maxSuccess ];
+	// Point[] expected = [Point(23,-10), Point(25,-9), Point(27,-5), Point(29,-6), Point(22,-6), Point(21,-7), Point(9,0), Point(27,-7), Point(24,-5), Point(25,-7), Point(26,-6), Point(25,-5), Point(6,8), Point(11,-2), Point(20,-5), Point(29,-10), Point(6,3), Point(28,-7), Point(8,0), Point(30,-6), Point(29,-8), Point(20,-10), Point(6,7), Point(6,4), Point(6,1), Point(14,-4), Point(21,-6), Point(26,-10), Point(7,-1), Point(7,7), Point(8,-1), Point(21,-9), Point(6,2), Point(20,-7), Point(30,-10), Point(14,-3), Point(20,-8), Point(13,-2), Point(7,3), Point(28,-8), Point(29,-9), Point(15,-3), Point(22,-5), Point(26,-8), Point(25,-8), Point(25,-6), Point(15,-4), Point(9,-2), Point(15,-2), Point(12,-2), Point(28,-9), Point(12,-3), Point(24,-6), Point(23,-7), Point(25,-10), Point(7,8), Point(11,-3), Point(26,-7), Point(7,1), Point(23,-9), Point(6,0), Point(22,-10), Point(27,-6), Point(8,1), Point(22,-8), Point(13,-4), Point(7,6), Point(28,-6), Point(11,-4), Point(12,-4), Point(26,-9), Point(7,4), Point(24,-10), Point(23,-8), Point(30,-8), Point(7,0), Point(9,-1), Point(10,-1), Point(26,-5), Point(22,-9), Point(6,5), Point(7,5), Point(23,-6), Point(28,-10), Point(10,-2), Point(11,-1), Point(20,-9), Point(14,-2), Point(29,-7), Point(13,-3), Point(23,-5), Point(24,-8), Point(27,-9), Point(30,-7), Point(28,-5), Point(21,-10), Point(7,9), Point(6,6), Point(21,-5), Point(27,-10), Point(7,2), Point(30,-9), Point(21,-8), Point(22,-7), Point(24,-9), Point(20,-6), Point(6,9), Point(29,-5), Point(8,-2), Point(27,-8), Point(30,-5), Point(24,-7) ];
+	// sort(expected);
+	// sort(successes);
+
+	// writeln("Difference 1: ", setDifference(expected, successes));
+	// writeln("Difference 2: ", setDifference(successes, expected));
+	// writeln(successes.length);
+	return [ maxSuccess, successes.length ];
 }
+
 
 void main() {
 
-	assert (solve("test") == [ 45 ]);
+	assert (solve("test") == [ 45, 112 ]);
 	writeln (solve("input"));
 }
