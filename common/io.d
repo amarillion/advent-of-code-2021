22029563dea88177b2466202fd1d38aa4ea22530
Martijn van Iersel <mvaniersel@gmail.com>
2022-01-17 17:14:54 +0100

    Easier file reading

--- common/io.d
+++ common/io.d
@@ -1,16 +1,9 @@
 module common.io;
 
-import std.stdio;
+import std.file;
 import std.string;
 
 string[] readLines(string fname) {
-	File file = File(fname, "rt");
-	string[] result = [];
-	while (!file.eof()) {
-		string line = chomp(file.readln()); 
-		result ~= line;
-	}
-	// Remove empty line...
-	if (result[$-1].length == 0) { result = result[0..$-1]; }
+	string[] result = readText(fname).stripRight.split('\n');
 	return result;
 }
